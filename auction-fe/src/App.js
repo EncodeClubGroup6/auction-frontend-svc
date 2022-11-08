f9fed35f86a8633d4cc4a93bcfea5b0aca236218
Paulo Almeida <paulo.roberto.a@hotmail.com>
2022-11-08 21:21:17 +0100

    Design Changes

--- auction-fe/src/App.js
+++ auction-fe/src/App.js
@@ -1,10 +1,12 @@
+import Form from "./components/Form"
 import MetamaskButton from "./components/MetamaskButton"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
-      <MetamaskButton />
+    <Navbar />
+      <Form />
     </>
   )
 }

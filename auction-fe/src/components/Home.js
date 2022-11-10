886aa931a607dc1da5b5be8738d0516c73c593de
Paulo Almeida <paulo.roberto.a@hotmail.com>
2022-11-10 01:30:59 +0100

    routes & component improvments Last changes

--- auction-fe/src/components/Home.js
+++ auction-fe/src/components/Home.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import Allowed from './Allowed'
 import CreateAuction from './CreateAuction'
 
 export default function Home() {
     return (
         <div>
-        {/* <CreateAuction /> */}
-        <Allowed />
+         <CreateAuction /> 
         </div>
     )
 }
